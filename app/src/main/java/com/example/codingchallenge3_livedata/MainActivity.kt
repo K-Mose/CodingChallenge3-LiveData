2c926ae3effd1b2db2ecdb20a6481e650b5b92a3
user <km_413@naver.com>
2021-07-14 00:33:58 +0900

    Code Challenge3 - LiveData

--- app/src/main/java/com/example/codingchallenge3_livedata/MainActivity.kt
+++ app/src/main/java/com/example/codingchallenge3_livedata/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
-import com.example.codingchellenge3_livedata.databinding.ActivityMainBinding
+import com.example.codingchallenge3_livedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
