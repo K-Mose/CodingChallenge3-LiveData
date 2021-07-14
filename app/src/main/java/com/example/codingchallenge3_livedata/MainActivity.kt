f668f8c3959b0092be2bbc0f4854251f3c409194
user <km_413@naver.com>
2021-07-14 22:52:22 +0900

    Integrate ViewModel with DataBinding

--- app/src/main/java/com/example/codingchallenge3_livedata/MainActivity.kt
+++ app/src/main/java/com/example/codingchallenge3_livedata/MainActivity.kt
@@ -12,9 +12,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var viewModelFactory: MainActivityViewModelFactory
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
         viewModelFactory = MainActivityViewModelFactory(0)
         viewModel = ViewModelProvider(this, viewModelFactory ).get(MainActivityViewModel::class.java)
+        /*
+        Integrate ViewModel with DataBinding
+        Live Data는 항상 activity, fragment, service의 lifecycle과 연관되기 떄문에
+        observer를 쓰지 않고 layout에 binding 하게되면
+        lifecycleOwner를 제공해야 한다.
+        여기서는 lifecycleOwner에 MainActivity를 lifecycleOwner로 할당한다.
+         */
+        binding.lifecycleOwner = this
         binding.viewModel = viewModel
         viewModel.countData.observe(this, {
             binding.tvCount.text = it.toString()
